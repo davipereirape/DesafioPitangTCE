81b652291289e946810c0441bdc5131b71e9578c
Davi Pereira <pereiradavipe@gmail.com>
2020-07-22 17:24:27 -0300

    Criar arquitetura/projeto

---
+++ src/main/java/com/pitang/desafiotce/DesafioPitangTceApplication.java
@@ -0,0 +1,13 @@
+package com.pitang.desafiotce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPitangTceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPitangTceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pitang/desafiotce/DesafioPitangTceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitang.desafiotce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPitangTceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
